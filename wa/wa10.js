2c2a631f651745161f4d1b07d94e2c188b91f67e
alliekamps <kaka8695@colorado.edu>
2024-03-21 12:49:34 -0600

    Update wa10.js

--- wa/wa10.js
+++ wa/wa10.js
@@ -2,10 +2,10 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day."
+const insertX = ["Looney tooney", "Flo Willy", "Tankity Tank"];
+const insertY = ["Puerto Vallerta", "the dog hotel", "Grandview"];
+const insertZ = ["rolled in the dirt", "doordashed Birdcall", "made a TikTok with Charlie Demilio"];
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
@@ -21,9 +21,10 @@ let xItem = randomValueFromArray(insertX);
 let yItem = randomValueFromArray(insertY);
 let zItem = randomValueFromArray(insertZ);
 
-newStory = newStory.replace(":insertx:", xItem);
-newStory = newStory.replace(":inserty:", yItem);
-newStory = newStory.replace(":insertz:", zItem);
+newStory = newStory.replace(":insertX:", xItem);
+newStory = newStory.replace(":insertY:", yItem);
+newStory = newStory.replace(":insertZ:", zItem);
+newStory = newStory.replace(":insertX:", xItem);
 
 
   if(customName.value !== '') {
